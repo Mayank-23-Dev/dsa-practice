c411d2892306159b161840880f6f3e61d6000e34
Mayank Dev <dmayank2323@gmail.com>
2026-08-27 20:42:05 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (50.02%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -6,7 +6,10 @@ public:
         for(int i=1;i<nums.size();i++)
         {
             if(nums[i]<=nums[i-1])
+            {
                 index=i;
+                break;
+            }
         }
         if(nums[index]<nums[0])
             return nums[index];
