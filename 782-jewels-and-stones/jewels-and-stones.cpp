ea9ad049ec7a61b1b9e97f2e7302a392d144216f
Mayank Dev <dmayank2323@gmail.com>
2026-09-04 03:38:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (62.79%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones)
+    {
+        int ans=0;
+        for(char i:jewels)
+        {
+            for(char j:stones)
+            {
+                if(i==j)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
