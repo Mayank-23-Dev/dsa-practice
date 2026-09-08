0db5ab3729572c0075278f129cc239efe8cc6a28
Mayank Dev <dmayank2323@gmail.com>
2026-09-08 10:02:31 +0530

    Time: 3 ms (62.52%) | Memory: 11.2 MB (82.99%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k)
+    {
+        if (num.size()==k)
+            return "0";
+        string ans = "";    
+        for (char c:num)
+        {
+            while (!ans.empty() && ans.back()>c && k>0)
+            {
+                ans.pop_back();
+                k--;
+            }
+            ans.push_back(c);
+        }
+        while (k>0 && !ans.empty())
+        {
+            ans.pop_back();
+            k--;
+        }
+        int start=0;
+        while (start<ans.size() && ans[start]=='0')
+            start++;
+        ans=ans.substr(start);
+        if(ans=="")
+            return "0";
+        return ans;
+    }
+};
